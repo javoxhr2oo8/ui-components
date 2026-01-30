a3c2523df1f2317bdd97ac07502a7f8922af14c6
javoxhr2oo8 <javohirblogg3r@gmail.com>
2026-01-30 21:54:49 +0300

    news

--- app/components/Ui/CardContent.vue
+++ app/components/Ui/CardContent.vue
@@ -11,7 +11,7 @@ const { item } = defineProps(['item'])
          <img src="https://miro.medium.com/v2/resize:fit:1200/1*qrHM3MBWI4LlWJdtFkR5CA.jpeg" alt="">
         <h2>{{ item }}. Card content</h2>
         <p>
-            20$
+            200 000 uzs
         </p>
         <Button>
             Buy now
